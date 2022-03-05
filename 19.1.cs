f0367a73f1d0b554aaf273a780aea37b6b88eeb1
zheng zhao <zzyzxb@hotmail.com>
2022-03-05 16:41:05 +0800

    添加项目文件。

---
+++ 19.1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //Type t1 = DateTime.Now.GetType();
+            //Type t2 = typeof(DateTime);
+            //Console.WriteLine(DateTime.Now.ToString());
+
+            Type stringType = typeof(string);
+        }
+    }
+}
